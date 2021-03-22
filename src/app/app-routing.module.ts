90c2aaa683256578b2443b325f17636db4bba8a4
priya421 <onlineprogramminglanguages@gmail.com>
2021-03-22 18:18:32 +0530

    up

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,13 @@ import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
 import { HomeComponent } from './home/home.component';
 
+
 const routes: Routes = [
-  {path:"",component:AboutComponent},
-  {path:"home",component:HomeComponent},
-  {path:"contact",component:ContactComponent}
+  {path:"",component:HomeComponent},
+  {path:'contact',component:ContactComponent},
+  {path:'about',component:AboutComponent}
 ];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './contact/contact.component';
 import { AboutComponent } from './about/about.component';
+import { HelloComponent } from './hello/hello.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     ContactComponent,
-    AboutComponent
+    AboutComponent,
+    HelloComponent
   ],
   imports: [
     BrowserModule,
